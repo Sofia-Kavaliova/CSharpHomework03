7720ffb948346cd79aec3023cb53d6495fdd1abb
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-17 14:44:27 +0300

    add decision task03

---
+++ task03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task 3
+
+Console.Write("Vvedite chislo: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = 1; i <=N; i++)
+{
+    Console.WriteLine(Math.Pow(i,3) + " ");
+}
