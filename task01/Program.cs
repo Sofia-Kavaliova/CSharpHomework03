ddf3107677d279b7cd362ad8d7027829e0949cb3
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-17 14:13:29 +0300

    fixed bugs task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,9 +1,9 @@
 ﻿// DZ 1
 
 Console.WriteLine("Vvedite piatiznachnoe chislo: ");
-string strNumber = Console.ReadLine();
+string? strNumber = Console.ReadLine();
 
-if (strNumber.Length ==5)
+if (strNumber?.Length ==5)
 {
     if (strNumber[0] == strNumber[4] && strNumber[1] == strNumber[3])
     {
