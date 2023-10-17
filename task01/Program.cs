0831aa50c9df4ad1f4f70e3817c184f0063a5d7a
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-17 14:08:32 +0300

    add decision task01

---
+++ task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// DZ 1
+
+Console.WriteLine("Vvedite piatiznachnoe chislo: ");
+string strNumber = Console.ReadLine();
+
+if (strNumber.Length ==5)
+{
+    if (strNumber[0] == strNumber[4] && strNumber[1] == strNumber[3])
+    {
+        Console.WriteLine("True");
+    }
+    else
+    {
+    Console.WriteLine("False");
+    }
+}
